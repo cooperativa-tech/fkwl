bab6cceda570b7bec5ef95f03b3deff11e03506a
José Sousa <josesousa9000@gmail.com>
2019-10-25 12:45:47 +0100

    Setup i18n

---
+++ src/i18n/pt.js
@@ -0,0 +1,21 @@
+module.exports = {
+  hello: 'Olá',
+  home1: `
+    O From Kibera With Love é um projeto criado pela portuguesa
+    Marta Baeta em Janeiro de 2013 que tem como objetivo dar
+    oportunidades de crescimento a crianças de Kibera, uma das
+    maiores favelas do mundo.
+  `,
+  home2: `
+    No Quénia ter-se acesso à educação é um luxo e a maioria das crianças
+    de Kibera não tem essa sorte, simplesmente porque muitas delas não têm
+    uma família que consiga assegurar o seu futuro.
+  `,
+  home3: `
+    Graças a amigos, conhecidos e estranhos que rapidamente se tornam
+    amigos, conseguimos garantir educação a mais de 100 crianças.
+  `,
+  speakWithUs: 'Para falar connosco, envia-nos um email para:',
+  donatives: 'Para donativos, aqui tens a informação que precisas:',
+  moreInfo: 'E aqui podes encontrar mais informação sobre nós:'
+};
